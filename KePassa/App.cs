545d16c082354f7bb75e671c814876a6fb02840a
Bobsans <mr.bobsans@gmail.com>
2024-10-27 15:22:56 +0300

    fix: shutdown behaviour

--- KePassa/App.cs
+++ KePassa/App.cs
@@ -7,16 +7,14 @@ namespace SecretStore;
 public class App: Application {
     public static IContainer Services { get; set; } = null!;
 
-    private readonly MainWindow _mainWindow;
-
     public App(MainWindow mainWindow) {
-        _mainWindow = mainWindow;
-        ShutdownMode = ShutdownMode.OnExplicitShutdown;
+        MainWindow = mainWindow;
+        ShutdownMode = ShutdownMode.OnMainWindowClose;
         Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("Ui/Styles.xaml", UriKind.Relative) });
     }
 
     protected override void OnStartup(StartupEventArgs e) {
         base.OnStartup(e);
-        _mainWindow.Show();
+        MainWindow!.Show();
     }
 }
